374095333503557f29825ab7d4e42417de264ecd
Karl Ziviani <73142421+Prince-projects@users.noreply.github.com>
2023-10-16 14:48:49 +1100

    Adding rcon func

--- minecraft_networking.py
+++ minecraft_networking.py
@@ -1,11 +1,18 @@
+import json
+
 from mctools import RCONClient
 
 HOST = 'localhost'  # Hostname of the Minecraft server
 PORT = 25575  # Port number of the RCON server
 rcon = RCONClient(HOST, port=PORT)
 
+
 def load_creds():
-    return
+    with open('creds.json') as f:
+        content = json.load(f)
+        return content['rcon']
+
+
 class MinecraftNetworking:
     def __init__(self, target, item, amount):
         self.item = item
@@ -13,6 +20,6 @@ class MinecraftNetworking:
         self.amount = amount
 
     def buy_command(self):
-        if rcon.login():
+        if rcon.login(load_creds()):
             print('give ' + self.target + ' ' + self.item + ' ' + str(self.amount))
             rcon.command('give ' + self.target + ' ' + self.item + ' ' + str(self.amount))
